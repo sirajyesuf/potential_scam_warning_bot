92846df94fd5f7e16c50e0d38932db255331b4f5
sirajyesuf <sirajyesuf762@gmail.com>
2022-03-28 09:18:12 +0300

    =handle telegram not found case in the token

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from decorators import bot_owners_only, bot_status_on
 import logging
 from keyboards import main_menu_markup, status_inline_btn, list_channel_group, channel_group_show
 from models import *
+
 log_format = logging.Formatter(
     "%(asctime)s - [%(name)s] [%(levelname)s]  %(message)s")
 logger = logging.getLogger()
@@ -29,27 +30,31 @@ bot = Client('bot',
              api_hash=api_hash,
              bot_token=config.BOT_TOKEN)
 
-
 contract_keyword = ['Address:']
-project_keyword = ['Contract Verified:',
-                   'Liquidity Locked:', 'Token Launching:', 'New Token Deployed:']
-title_keywords = ['Rugpull', 'Honeypot',
-                  'Balance Swapper', 'Liquidity Drainer', 'Hidden Mint']
+project_keyword = [
+    'Contract Verified:', 'Liquidity Locked:', 'Token Launching:',
+    'New Token Deployed:'
+]
+title_keywords = [
+    'Rugpull', 'Honeypot', 'Balance Swapper', 'Liquidity Drainer',
+    'Hidden Mint'
+]
 
 link_keywords = ['Explore:', 'Telegram?']
+tg_not_found = "Explore: Telegram 🤷‍"
 scamcode_keywords = ['Scam Check:', 'scam code detected']
 
 
 def get_telegram_link(caption_entities):
     for en in caption_entities:
-        if(en.type == 'text_link' and en.url.count('/') == 3):
+        if (en.type == 'text_link' and en.url.count('/') == 3):
             return en.url
 
 
 def clean_signal(signal):
     signal = signal.split("\n")
     for i in signal:
-        if(i == ""):
+        if (i == ""):
             signal.remove(i)
     return signal
 
@@ -57,14 +62,16 @@ def clean_signal(signal):
 def get_dict(message):
     caption = clean_signal(message.caption)
     dict = {}
+    dict['link'] = "Telegram not found."
     for i in caption:
-        if([kw for kw in title_keywords if kw in i]):
+        if ([kw for kw in title_keywords if kw in i]):
             dict['title'] = [kw for kw in title_keywords if kw in i][0]
-        if([kw for kw in project_keyword if kw in i]):
+        if ([kw for kw in project_keyword if kw in i]):
             dict['project'] = i.split(":")[1].strip()
-        if([kw for kw in contract_keyword if kw in i]):
+        if ([kw for kw in contract_keyword if kw in i]):
             dict['contract'] = i.split(":")[1].strip()
-        if([kw for kw in link_keywords if kw in i]):
+        if (len([kw for kw in link_keywords if kw in i]) == 2
+                and tg_not_found not in i):
             dict['link'] = get_telegram_link(message.caption_entities)
     return dict
 
@@ -79,7 +86,7 @@ def get_out_put_signal(message):
     _contract = f"<b>Contract:</b> {dict['contract']}"
     _link = f"<b>Link:</>{dict['link']}"
     _bottom = sett['template']
-    if(dict.get('link')):
+    if (dict.get('link')):
         return f"{_header}\n\n{_title}\n\n{_project}\n\n{_link}\n{_contract}\n\n{_bottom}"
     else:
         return f"{_header}\n\n{_title}\n\n{_project}\n\n{_contract}\n\n{_bottom}"
@@ -88,15 +95,16 @@ def get_out_put_signal(message):
 @user.on_message(filters.chat(config.SOURCE_CHANNEL) & filters.forwarded)
 @bot_status_on
 def scam_check(user, message):
-    if([kw for kw in title_keywords if kw in message.caption] and [kw for kw in scamcode_keywords if kw in message.caption] and [kw for kw in link_keywords if kw in message.caption]):
+    print(message)
+    if ([kw for kw in title_keywords if kw in message.caption]
+            and [kw for kw in scamcode_keywords if kw in message.caption]
+            and [kw for kw in link_keywords if kw in message.caption]):
         out_put_signal = get_out_put_signal(message)
         for des in all_chats():
-            user.send_message(
-                chat_id=int(des['id']),
-                text=out_put_signal,
-                disable_web_page_preview=True,
-                parse_mode='html'
-            )
+            user.send_message(chat_id=int(des['id']),
+                              text=out_put_signal,
+                              disable_web_page_preview=True,
+                              parse_mode='html')
         print("successfull forwarding....")
     else:
         print("ignore the signal")
@@ -105,11 +113,9 @@ def scam_check(user, message):
 @bot.on_message(filters.command('start') | filters.regex('🏠Home'))
 @bot_owners_only
 def start(bot, message):
-    bot.send_message(
-        chat_id=message.from_user.id,
-        text=f"Hello {message.from_user.first_name}!👋",
-        reply_markup=main_menu_markup
-    )
+    bot.send_message(chat_id=message.from_user.id,
+                     text=f"Hello {message.from_user.first_name}!👋",
+                     reply_markup=main_menu_markup)
 
 
 # @bot.on_message(filters.regex('🏠Home'))
@@ -133,13 +139,12 @@ def settings(bot, message):
     sett = get_setting()
     reply_markup = status_inline_btn()
     note = "✅ bot forwarding enabled."
-    if(not sett['status']):
+    if (not sett['status']):
         note = "❌ bot forwarding disabled."
     bot.send_message(
         chat_id=message.from_user.id,
         text=f"⚙️Setting\n\n{note}\n\n👇set the status of the bot.",
-        reply_markup=reply_markup
-    )
+        reply_markup=reply_markup)
 
 
 static_data_filter = filters.create(
@@ -154,14 +159,13 @@ def set_bot_status_setting(bot, callback_query):
     reply_markup = status_inline_btn()
     sett = get_setting()
     note = "✅ bot forwarding enabled."
-    if(not sett['status']):
+    if (not sett['status']):
         note = "❌ bot forwarding disabled."
     bot.edit_message_text(
         chat_id=callback_query.from_user.id,
         message_id=callback_query.message.message_id,
         text=f"⚙️Setting\n\n{note}\n\n👇set the status of the bot.",
-        reply_markup=reply_markup
-    )
+        reply_markup=reply_markup)
 
 
 # @bot.on_message(filters.forwarded)
@@ -188,16 +192,15 @@ def set_bot_status_setting(bot, callback_query):
 @bot.on_message(filters.regex('Channels'))
 def list_chats(bot, message):
     reply_markup = list_channel_group()
-    if(reply_markup['inline_keyboard']):
-        bot.send_message(
-            chat_id=message.from_user.id,
-            text="list of channel/group",
-            reply_markup=reply_markup
-        )
+    if (reply_markup['inline_keyboard']):
+        bot.send_message(chat_id=message.from_user.id,
+                         text="list of channel/group",
+                         reply_markup=reply_markup)
     else:
         bot.send_message(
             chat_id=message.from_user.id,
-            text="No channel or group.\nyou can create by using the command below\n\n /addgroup username",
+            text=
+            "No channel or group.\nyou can create by using the command below\n\n /addgroup username",
         )
 
 
@@ -206,47 +209,43 @@ def show_chat(bot, callback_query):
     chat_id = callback_query.data
     _chat = get_chat(chat_id)
     text = f"{_chat['type']} Detail\n\nID= {_chat['id']}\nTitle= {_chat['title']}\nUsername: @{_chat['username']}"
-    bot.edit_message_text(
-        chat_id=callback_query.from_user.id,
-        message_id=callback_query.message.message_id,
-        text=text,
-        reply_markup=channel_group_show(_chat['id'])
-    )
+    bot.edit_message_text(chat_id=callback_query.from_user.id,
+                          message_id=callback_query.message.message_id,
+                          text=text,
+                          reply_markup=channel_group_show(_chat['id']))
 
 
 @bot.on_callback_query(filters.regex('back|^delete-.*$'))
 def delete_back(bot, callback_query):
-    if(callback_query.data == 'back'):
+    if (callback_query.data == 'back'):
         reply_markup = list_channel_group()
-        if(reply_markup['inline_keyboard']):
-            bot.edit_message_text(
-                chat_id=callback_query.from_user.id,
-                message_id=callback_query.message.message_id,
-                text="list of channel/group",
-                reply_markup=reply_markup
-            )
+        if (reply_markup['inline_keyboard']):
+            bot.edit_message_text(chat_id=callback_query.from_user.id,
+                                  message_id=callback_query.message.message_id,
+                                  text="list of channel/group",
+                                  reply_markup=reply_markup)
         else:
             bot.edit_message_text(
                 chat_id=callback_query.from_user.id,
                 message_id=callback_query.message.message_id,
-                text="No channel or group.\nyou can create by using the command below\n\n /addgroup username",
+                text=
+                "No channel or group.\nyou can create by using the command below\n\n /addgroup username",
             )
-    if('delete' in callback_query.data):
+    if ('delete' in callback_query.data):
         chat_id = f"-{callback_query.data.split('-')[1]}"
         delete_chat(chat_id)
         reply_markup = list_channel_group()
-        if(reply_markup['inline_keyboard']):
-            bot.edit_message_text(
-                chat_id=callback_query.from_user.id,
-                message_id=callback_query.message.message_id,
-                text="list of channel/group",
-                reply_markup=reply_markup
-            )
+        if (reply_markup['inline_keyboard']):
+            bot.edit_message_text(chat_id=callback_query.from_user.id,
+                                  message_id=callback_query.message.message_id,
+                                  text="list of channel/group",
+                                  reply_markup=reply_markup)
         else:
             bot.edit_message_text(
                 chat_id=callback_query.from_user.id,
                 message_id=callback_query.message.message_id,
-                text="No channel or group.\nyou create just by forwarding any post from the channel or group to this bot.",
+                text=
+                "No channel or group.\nyou create just by forwarding any post from the channel or group to this bot.",
             )
 
 
@@ -260,7 +259,7 @@ def add_group_chat(bot, message):
         'title': _chat.title,
         'username': _chat.username
     }
-    if(store_chats(data)):
+    if (store_chats(data)):
         text = f"✅ the {_chat.type} @{_chat.username} created sucessfully."
 
     else:
@@ -285,30 +284,26 @@ def help(bot, message):
 def template(bot, message):
     sett = get_setting()
     text = f"if you want to change this template just enter the new one\n if not /cancel \n\n{sett['template']}"
-    bot.send_message(
-        chat_id=message.from_user.id,
-        text=text,
-        disable_web_page_preview=True,
-        parse_mode='html'
-    )
-    conv = {
-        'que': '/template',
-        'ans': 'update_template'
-    }
+    bot.send_message(chat_id=message.from_user.id,
+                     text=text,
+                     disable_web_page_preview=True,
+                     parse_mode='html')
+    conv = {'que': '/template', 'ans': 'update_template'}
     create_conv(conv)
 
 
-@bot.on_message(filters.text & ~filters.command(['cancel', 'template', 'addgroup']))
+@bot.on_message(filters.text
+                & ~filters.command(['cancel', 'template', 'addgroup']))
 def all_text(bot, message):
     conv = get_conv()
     # print(conv)
     # OrderedDict([('id', 1), ('que', '/template'), ('ans', 'update_template')])
-    if(conv and conv['que'] == '/template'):
+    if (conv and conv['que'] == '/template'):
         text = message.text
         entities = message.entities
-        if(entities):
+        if (entities):
             for ent in entities:
-                if(ent['type'] == 'text_link'):
+                if (ent['type'] == 'text_link'):
                     start = ent['offset']
                     end = ent['offset'] + ent['length']
                     old = text[start:end]
@@ -336,11 +331,8 @@ def cancel(bot, message):
 _channel = "<a href='https://t.me/shartanentryportal'>SHARTAN ARMY</a>"
 _bottom = f"Ask the dev BEFORE buying!\nDon't get trapped!\n\n{_channel} $SHARTAN #NFA"
 
-if(get_setting() == None):
-    sett = {
-        'status': True,
-        'template': _bottom
-    }
+if (get_setting() == None):
+    sett = {'status': True, 'template': _bottom}
     store_setting(sett)
 logger.info('POTENTIAL SCAM WARNING BOT START 🚀')
 user.start()
